81bf996515ec0a1ce0a69c8872a90407896eaa4d
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-13 13:07:23 +0400

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__, template_folder='./template')
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        f = open('script.sh', 'w')
+        f = open('sh-files/script.sh', 'w')
         checked =  request.form.getlist('check')
         for item in checked:
             if item in checked:
@@ -25,7 +25,7 @@ def index():
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():
-    path = 'script.sh'
+    path = 'sh-files/script.sh'
     return send_file(path, as_attachment=True)
 
 

bc8eda94d48cd8239908a4c7be598aa31ce95bec
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-17 00:42:11 +0400

    update

--- sh-files/script.sh
+++ sh-files/script.sh
@@ -1,25 +1,42 @@
-#start of docker installation
-sudo apt-get update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce
-#end of docker installation
-#
-#start of portainer in docker installation
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-#end of portainer in docker installation
-#
-#start of jupyterhub installation
-sudo apt update
-sudo apt install -y python
-sudo apt install -y npm nodejs
-sudo apt install -y python3-pip
-cd /
-sudo python3 -m pip install jupyterhub
-sudo npm install -g configurable-http-proxy
-sudo python3 -m pip install notebook
-#end of jupyterhub installation
+#start of moodle with mariadb in docker installation
+wget [put a link to raw content]
+read -p 'Do you want to allow empty passwords? y/n ' ans
+if [$ans = "n"]
+then
+    sed -i 's/ALLOW_EMPTY_PASSWORD=yes/ALLOW_EMPTY_PASSWORD=no' moodle.yml
+else
+    echo "next question"
+fi
+read -p 'Do you need a password for mariadb? y/n ' ans
+if [$ans = "y"] 
+then
+    read -p 'Enter the root password for mariadb: ' pass
+    sed -i 's/#- MARIADB_ROOT_PASSWORD=aramaki404/- MARIADB_ROOT_PASSWORD=$pass' moodle.yml
+    read -p 'Enter the password for mariadb: ' pass2
+    sed -i 's/#- MARIADB_PASSWORD=aramaki404/- MARIADB_PASSWORD=$pass2' moodle.yml
+    sed -i 's/#- MOODLE_DATABASE_PASSWORD=aramaki404/- MOODLE_DATABASE_PASSWORD=$pass2' moodle.yml
+else 
+    echo 'next question'
+fi
+read -p 'Do you need a moodle password? y/n ' ans
+if [$ans = "y"]
+then
+    read -p 'Enter the password: ' pass
+    sed -i 's/#- MOODLE_PASSWORD=aramaki404/- MOODLE_PASSWORD=$pass' moodle.yml
+else
+    echo 'next question'
+fi
+read -p 'Do you want to change ports on host? y/n ' ans
+if [$ans = "y"]
+then
+    read -p 'port 80 change to ' port1 
+    sed -i 's/- '80:8080'/- '$port1:8080' moodle.yml
+    read -p 'port 8443 change to ' port2
+    sed -i 's/- '8443:8443'/- '$port2:8443'' moodle.yml
+else
+    echo 'No changes to ports'
+fi
+mv moodle.yml docker-compose.yml
+sudo docker compose up
+#end of installation
 #
